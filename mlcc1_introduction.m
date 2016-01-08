60f4915e0ef46c87056e484ccd6cbca05b14e04f
Alistair Johnson <alistairewj@gmail.com>
2016-01-08 12:45:50 -0500

    formatting/a few more comments

--- mlcc1_introduction.m
+++ mlcc1_introduction.m
@@ -5,28 +5,34 @@ javaclasspath('sqlite-jdbc-3.8.11.2.jar') % use this for SQLite
 
 % STEP 2: Connect to the Database
 conn = database('','','',...
-    'org.sqlite.JDBC',['jdbc:sqlite:' pwd filesep 'data' filesep 'mimicdata.sqlite']);
+    'org.sqlite.JDBC',['jdbc:sqlite:' pwd filesep 'data' filesep 'mimiciii_v1_3_mini.sqlite']);
 
 %% Take a look at "mlcc-query-1.sql" - this extracts a single value for each patient
 % More specifically, it extracts the highest heart rate
-% See if you can add in the highest respiratory rate and highest GCS
-% Then, run the query here to get the results.
+query = makeQuery('mlcc-query-1.sql');
+data = fetch(conn,query);
+
+%% Plot a histogram of the highest heart rate values
+
 
 %% Plot the highest heart rate against the highest respiratory rate
 
 
+%% See if you can add in the highest GCS
+% Then, run the query here to get the results.
+query = makeQuery(''); % put the filename here
+data = fetch(conn,query);
+
 
 %% Plot the highest heart rate against the highest respiratory rate, colouring by outcome
-% The patient outcome is stored in "HOSPITAL_EXPIRE_FLAG" - the 5th column
-
-
-
-%% Box-plot the heart rate and respiratory rates
-% How could you modify "mlcc-query-1.sql" to prevent outliers?
+% The patient outcome is stored in "HOSPITAL_EXPIRE_FLAG" - the 4th column
 
 
 %% Plot the highest heart rate against the highest GCS, colouring by outcome
 % Which variable do you feel discriminates mortality better?
+
+
+%% What other variables could you add which might help?
 
 
 %% Build a logistic regression to classify mortality
